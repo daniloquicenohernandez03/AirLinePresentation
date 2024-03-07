16d1e502f7ffe8b496725a69138fbb74a0dbf67a
Danny16 <daniloquicenohernandez0216@gmail.com>
2024-03-07 11:57:20 -0500

    Requirement finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgOptimizedImage } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component'
 import { NavBarComponent } from './components/navbar/navbar.component';
@@ -7,10 +7,28 @@ import { NavBarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FooterComponent, NavBarComponent],
+  imports: [RouterOutlet, CommonModule, FooterComponent, NavBarComponent, NgFor, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'AirLine';
+
+  public images = [
+    {
+      name: 'Chicago',
+      img: './assets/img/chicago1.jpg',
+      price: 'From $600.00'
+    },
+    {
+      name: 'Porto',
+      img: './assets/img/porto.jpg',
+      price: 'From $600.00'
+    },
+    {
+      name: 'Chicago',
+      img: './assets/img/chicago2.jpg',
+      price: 'From $600.00'
+    },
+]
 }
